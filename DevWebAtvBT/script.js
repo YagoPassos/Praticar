c42e5f1dd38f3ba52e62be0d269f9a960c5361f9
Yago passos <yp.anjos@hotmail.com>
2019-10-01 00:37:03 -0300

    atualizado

--- DevWebAtvBT/script.js
+++ DevWebAtvBT/script.js
@@ -27,40 +27,41 @@ for (i = 0; i < img.length; i++) {
     });
 }
 
-var banner1 = document.querySelector('.banner1');
-var banner2 = document.querySelector('.banner2');
+var bgBn1 = document.querySelector('.bgBn1');
+var bgBn2 = document.querySelector('.bgBn2');
+var imgBanner1 = document.querySelector('.imgBanner1');
+var imgBanner2 = document.querySelector('.imgBanner2');
 
-banner1.addEventListener("mouseenter", function () {
-    banner1.style.zIndex = "2000";
-    banner1.style.left = "110px";
-    banner1.style.width = "800px";
-    banner1.style.transition = "1000ms";
+bgBn1.addEventListener("mouseenter", function () {
+
+    bgBn1.style.transition = "500ms";
+    bgBn1.style.filter= "blur(2px)";
+    imgBanner1.style.right= "120px";
+    
 });
-banner1.addEventListener("mouseout", function () {
-    banner1.style.zIndex = "0";
-    banner1.style.left = "30px";
-    banner1.style.width = "700px";
-    banner1.style.transition = "1000ms";
+bgBn1.addEventListener("mouseout", function () {
+    bgBn1.style.transition = "500ms";
+    bgBn1.style.filter= "blur(0)";
+    imgBanner1.style.right= "50px";
+});
+bgBn2.addEventListener("mouseenter", function () {
+
+    bgBn2.style.transition = "500ms";
+    bgBn2.style.filter= "blur(2px)";
+    imgBanner2.style.right= "120px";
+    
+    
+});
+bgBn2.addEventListener("mouseout", function () {
+    bgBn2.style.transition = "500ms";
+    bgBn2.style.filter= "blur(0)";
+    imgBanner2.style.right= "50px";
 });
 
-banner2.addEventListener("mouseenter", function () {
-    banner2.style.zIndex = "2000";
-    banner2.style.right = "150px";
-    banner2.style.bottom = "200px";
-    banner2.style.width = "430px";
-    banner2.style.transition = "1000ms";
-});
-banner2.addEventListener("mouseout", function () {
-    banner2.style.zIndex = "0";
-    banner2.style.right = "30px";
-    banner2.style.bottom = "200px";
-    banner2.style.width = "350px";
-    banner2.style.transition = "1000ms";
-});
 
 function initMap() {
     var salvador = { lat: -12.97111, lng: -38.51083 };
 
-    var map = new google.maps.Map(document.getElementById('map'), { zoom: 6, center: salvador });
+    var map = new google.maps.Map(document.getElementById('map'), { zoom: 5, center: salvador });
     var marker = new google.maps.Marker({ position: salvador, map: map });
 }
